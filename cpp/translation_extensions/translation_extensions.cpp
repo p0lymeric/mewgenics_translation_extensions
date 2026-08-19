26f87f4aba6f407bcc390cc2852b048faee9ab6f
polymeric <229394044+p0lymeric@users.noreply.github.com>
2026-08-18 23:57:37 -0400

    Remove translation counter and enable debug logging.

--- cpp/translation_extensions/translation_extensions.cpp
+++ cpp/translation_extensions/translation_extensions.cpp
@@ -1,12 +1,11 @@
 #include "amoeboid.hpp"
-// #include "utilities/debug_console.hpp"
+#include "utilities/debug_console.hpp"
 #include "utilities/function_hook.hpp"
-// #include "utilities/strings.hpp"
+#include "utilities/strings.hpp"
 #include "utilities/portal.hpp"
 #include "types/msvc.hpp"
 
 #include <cstdint>
-// #include <cwchar>
 
 // Translation Extensions
 //
@@ -27,7 +26,6 @@ enum struct TranslationOverrideState {
 
 struct TrapPrivateState {
     TranslationOverrideState to_state = TranslationOverrideState::Inactive;
-    uint32_t to_call_downcounter = 0;
     CatData *to_catdata = nullptr;
 };
 
@@ -44,35 +42,32 @@ MAKE_SHOOK(0, ADDRESS_glaiel__CatAdventureCutscene__init,
     void, __cdecl, glaiel__CatAdventureCutscene__init,
     CatAdventureCutscene *thiss, void *config
 ) {
+    D::debug("CatAdventureCutscene::init");
     P.to_state = TranslationOverrideState::ValidForCatDataCapture;
-    P.to_call_downcounter = 1;
     glaiel__CatAdventureCutscene__init_hook.orig(thiss, config);
     P.to_state = TranslationOverrideState::Inactive;
-    // D::debug("CatAdventureCutscene::init");
 }
 
 MAKE_SHOOK(0, ADDRESS_glaiel__BossIntroCutscene__init,
     void, __cdecl, glaiel__BossIntroCutscene__init,
     BossIntroCutscene *thiss, MsvcReleaseModeXString *arg2, MsvcReleaseModeXString *arg3
 ) {
+    D::debug("BossIntroCutscene::init");
     P.to_state = TranslationOverrideState::ValidForCatDataCapture;
-    // this is unstable across updates
-    P.to_call_downcounter = 4;
     glaiel__BossIntroCutscene__init_hook.orig(thiss, arg2, arg3);
     P.to_state = TranslationOverrideState::Inactive;
-    // D::debug("BossIntroCutscene::init");
 }
 
 MAKE_SHOOK(0, ADDRESS_maybe_CatDatabase_get_cat_data,
     CatData *, __cdecl, maybe_CatDatabase_get_cat_data,
     CatDatabase *thiss, int64_t sql_key
 ) {
+    // D::debug("maybe_CatDatabase_get_cat_data");
     CatData *cat = maybe_CatDatabase_get_cat_data_hook.orig(thiss, sql_key);
     if(P.to_state == TranslationOverrideState::ValidForCatDataCapture) {
         P.to_state = TranslationOverrideState::ValidForOverride;
         P.to_catdata = cat;
     }
-    // D::debug("maybe_CatDatabase_get_cat_data");
     return cat;
 }
 
@@ -82,52 +77,38 @@ MAKE_SHOOK(0, ADDRESS_maybe_StringsDatabase__do_translation,
 ) {
     maybe_StringsDatabase__do_translation_hook.orig(thiss, p_writeback, p_key, try_recurse_mfn);
     if(P.to_state == TranslationOverrideState::ValidForOverride) {
-        // D::debug("maybe_StringsDatabase__do_translation: {} -> {}", p_key->as_native_string_view(), convert_utf16_wstring_to_utf8_string(p_writeback->as_native_wstring_view()));
-        if(P.to_call_downcounter == 1) {
-            // MsvcReleaseModeXString *lang_code = reinterpret_cast<MsvcReleaseModeXString *>(reinterpret_cast<uint8_t *>(thiss) + STRINGSDATABASE_LANGCODE_OFFSET);
-            std::wstring gender;
-            bool try_alt_translation;
-            std::string alt_translation_key;
-            switch(*reinterpret_cast<int32_t *>(reinterpret_cast<uint8_t *>(P.to_catdata) + CATDATA_SEX_OFFSET)) {
-                case 0:
-                    gender = L"male";
-                    try_alt_translation = true;
-                    alt_translation_key = std::format("{}_M", p_key->as_native_string_view());
-                    break;
-                case 1:
-                    gender = L"female";
-                    try_alt_translation = true;
-                    alt_translation_key = std::format("{}_F", p_key->as_native_string_view());
-                    break;
-                case 2:
-                    gender = L"neutral";
-                    try_alt_translation = true;
-                    alt_translation_key = std::format("{}_N", p_key->as_native_string_view());
-                    break;
-                default:
-                    gender = L"unknown";
-                    try_alt_translation = false;
-                    break;
-            }
-            bool had_alt_translation = false;
-            if(try_alt_translation) {
-                MsvcReleaseModeXString alt_translation_key_host;
-                alt_translation_key_host.construct(alt_translation_key.data(), alt_translation_key.size());
-                had_alt_translation = maybe_StringsDatabase__has_translation(thiss, &alt_translation_key_host);
-                if(had_alt_translation) {
-                    p_writeback->destroy();
-                    maybe_StringsDatabase__do_translation_hook.orig(thiss, p_writeback, &alt_translation_key_host, false);
-                }
-                alt_translation_key_host.destroy();
-            }
-            // std::wstring writeback_override = std::format(L"({} {} {}) {}", gender, try_alt_translation, had_alt_translation, p_writeback->as_native_wstring_view());
-            // p_writeback->resize(writeback_override.size(), L'\0');
-            // std::wmemcpy(p_writeback->begin(), writeback_override.data(), writeback_override.size());
-            // D::debug("maybe_StringsDatabase__do_translation OVERRIDE: {}", convert_utf16_wstring_to_utf8_string(p_writeback->as_native_wstring_view()));
+        D::debug("Translation: {} -> {}", p_key->as_native_string_view(), convert_utf16_wstring_to_utf8_string(p_writeback->as_native_wstring_view()));
+        bool try_alt_translation;
+        std::string alt_translation_key;
+        switch(*reinterpret_cast<int32_t *>(reinterpret_cast<uint8_t *>(P.to_catdata) + CATDATA_SEX_OFFSET)) {
+            case 0:
+                try_alt_translation = true;
+                alt_translation_key = std::format("{}_M", p_key->as_native_string_view());
+                break;
+            case 1:
+                try_alt_translation = true;
+                alt_translation_key = std::format("{}_F", p_key->as_native_string_view());
+                break;
+            case 2:
+                try_alt_translation = true;
+                alt_translation_key = std::format("{}_N", p_key->as_native_string_view());
+                break;
+            default:
+                try_alt_translation = false;
+                break;
         }
-        P.to_call_downcounter--;
-        if(P.to_call_downcounter == 0) {
-            P.to_state = TranslationOverrideState::Inactive;
+        bool had_alt_translation = false;
+        if(try_alt_translation) {
+            MsvcReleaseModeXString alt_translation_key_host;
+            alt_translation_key_host.construct(alt_translation_key.data(), alt_translation_key.size());
+            had_alt_translation = maybe_StringsDatabase__has_translation(thiss, &alt_translation_key_host);
+            D::debug("Override key exists?: {} -> {}", alt_translation_key, had_alt_translation);
+            if(had_alt_translation) {
+                p_writeback->destroy();
+                maybe_StringsDatabase__do_translation_hook.orig(thiss, p_writeback, &alt_translation_key_host, false);
+                D::debug("Override result: {} -> {}", alt_translation_key, convert_utf16_wstring_to_utf8_string(p_writeback->as_native_wstring_view()));
+            }
+            alt_translation_key_host.destroy();
         }
     }
     return p_writeback;
